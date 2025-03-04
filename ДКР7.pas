c7be49cec8f24fb4e7cc4d8b664a49705d6334d8
its 21 <alexleo.space@gmail.com>
2025-03-04 21:34:04 +0300

    Update ДКР7.pas

--- ДКР7.pas
+++ ДКР7.pas
@@ -1,20 +1,28 @@
-﻿uses GraphABC;
- 
-const sinA = sin(pi/4); cosA = cos(pi/4);
- 
-procedure r(n : integer; x0,y0,x1,y1 : real);
+uses GraphABC; 
+
+const 
+  sinA = sin(pi/4);  // вычисляем синус угла 45 градусов
+  cosA = cos(pi/4);  // вычисляем косинус угла 45 градусов
+
+procedure r(n : integer; x0,y0,x1,y1 : real); // параметризация
 begin
-  if n = 0 then line(Round(x0),Round(y0),Round(x1),Round(y1))
+  if n = 0 then  // база рекурсии если n равно 0, рисуем линию
+    line(Round(x0), Round(y0), Round(x1), Round(y1))
   else
     begin
-      var xt := cosA*((x1-x0)*cosA-(y1-y0)*sinA)+x0;
-      var yt := cosA*((x1-x0)*sinA+(y1-y0)*cosA)+y0;
-      r(n-1, x0,y0,xt,yt);r(n-1, xt,yt,x1,y1);
+      // координаты новой точки после поворота
+      var xt := cosA * ((x1 - x0) * cosA - (y1 - y0) * sinA) + x0;
+      var yt := cosA * ((x1 - x0) * sinA + (y1 - y0) * cosA) + y0;
+      
+      // декомпозиция
+      r(n-1, x0, y0, xt, yt);  // первый отрезок
+      r(n-1, xt, yt, x1, y1);  // второй отрезок
     end;
 end;
- 
+
 begin
-  Window.SetSize(800,700);
+  Window.SetSize(800, 700); 
+ 
   r(readlninteger('введите n'), Window.Width div 4, Window.Height div 3, Window.Width - Window.Width div 4, Window.Height div 3);
-  Redraw;
+  Redraw; 
 end.
